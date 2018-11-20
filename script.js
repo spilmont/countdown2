7d81914f7e70ccbb888481f9c99b3c7f99d4f25e
spilmont <dd_spilmont@hotmail.fr>
2018-11-20 23:04:35 +0100

    version meche

--- script.js
+++ script.js
@@ -1,29 +1,106 @@
-var heure =1;
-var minute =0;
-var seconde =5;
+var heure = 0;
+var minute = 0;
+var seconde = 30;
+var temps = setTimeout(CaR, 1000);
 
-document.getElementById('heure').innerHTML=heure;
-document.getElementById('seconde').innerHTML=seconde;
-document.getElementById('minute').innerHTML=minute;
-
-function CaR(){
-    document.getElementById('seconde').innerHTML=seconde;
+function CaR() {
     seconde--;
-    if (seconde < 0){
+
+
+    if (seconde < 0) {
         minute--;
-        document.getElementById('minute').innerHTML=minute;
+
         seconde = 59;
     }
-    else if (minute < 0) {
+    if (minute < 0) {
         heure--;
+
+
         minute = 59;
-        document.getElementById('heure').innerHTML=heure;
         console.log(minute);
     }
+    if (heure < 0) {
+        heure = 0;
+    }
+    document.getElementById('seconde').innerHTML = seconde;
+    document.getElementById('minute').innerHTML = minute;
+    document.getElementById('heure').innerHTML = heure;
+    temps = setTimeout(CaR, 1000);
 
+    if (heure == 0 && minute == 0 && seconde == 0) {
+        clearTimeout(temps);
+        alert("BOUM!!!!!");
+    }
 
-    setTimeout(CaR,1000);
+    if (seconde <= 10) {
+        document.body.style.animation = "dd 2s infinite";
+    }
+    if (seconde == 0) {
+        document.body.style.backgroundColor = "darkred";
+        document.body.style.animation = "none";
+    }
+
+    var tempsTotal = (heure*3600)+(minute*60)+seconde;
+    document.getElementById('mechebox').style.width = tempsTotal +"px";
 
 
 }
-setTimeout(CaR,1000);
+
+
+document.getElementById('stop').addEventListener("click", function () {
+    clearTimeout(temps, 5);
+
+
+});
+
+document.getElementById('play').addEventListener("click", function () {
+    clearTimeout(temps);
+    setTimeout(CaR, 1000);
+
+
+});
+
+function user() {
+    var valeurS = document.getElementById('sUser').value;
+    var valeurM = document.getElementById('mUser').value;
+    var valeurH = document.getElementById('hUser').value;
+
+    if (valeurS) {
+        seconde = valeurS;
+    } else {
+        seconde = 0;
+    }
+
+    if (valeurM) {
+        minute = valeurM;
+    } else {
+        minute = 0;
+    }
+
+    if (valeurH) {
+        heure = valeurH;
+    } else {
+        heure = 0;
+    }
+}
+
+document.getElementById("start").addEventListener("click", function () {
+    user();
+    console.log("sec" + seconde);
+    console.log("min" + minute);
+    console.log("hrs" + heure);
+});
+document.getElementById('reset').addEventListener("click", function () {
+    seconde = 0;
+    minute = 0;
+    heure =0;
+
+    document.getElementById('seconde').innerHTML = seconde;
+    document.getElementById('minute').innerHTML = minute;
+    document.getElementById('heure').innerHTML = heure;
+
+    clearTimeout(temps);
+});
+
+
+
